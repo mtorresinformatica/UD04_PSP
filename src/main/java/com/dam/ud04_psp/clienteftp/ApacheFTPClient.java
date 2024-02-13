8ebc5d922fcb0362cdf79e63698eddb3694390c5
mtorresinformatica <89740575+mtorresinformatica@users.noreply.github.com>
2024-02-13 12:36:51 +0100

    Ejemplos de clase de uso de un Cliente FTP

---
+++ src/main/java/com/dam/ud04_psp/clienteftp/ApacheFTPClient.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dam.ud04_psp.clienteftp;
+
+import java.io.IOException;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+/**
+ *
+ * @author DAM
+ */
+public class ApacheFTPClient {
+
+    private String server;
+    private int port;
+    private String user;
+    private String password;
+    private FTPClient ftp;
+
+    public ApacheFTPClient(String server, int port, String user, String password) {
+        this.server = server;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+    }
+
+    void open() throws IOException {
+        ftp = new FTPClient();
+
+        ftp.connect(server, port);
+        int reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new IOException("Exception in connecting to FTP Server");
+        }
+
+        ftp.login(user, password);
+    }
+
+    void close() throws IOException {
+        ftp.disconnect();
+    }
+
+    public static void main(String[] args) throws IOException {
+        ApacheFTPClient client = new ApacheFTPClient("localhost", 21, "usuario1", "usu1");
+        client.open();
+        client.close();
+    }
+}
